7efedb627951044054b0b7b7d897a98b5026ab57
kou1046 <iwashiro0517@yahoo.co.jp>
2023-02-20 05:59:27 +0900

    ページの追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <header className="container mx-auto bg-gradient-to-r from-cyan-200 to-blue-400 p-10 text-center">
+        <h1>Beat walker</h1>
+        <h2>歩く，もしくはクリックしてビートを刻もう．</h2>
+      </header>
+      <Component {...pageProps} />;
+    </>
+  );
 }
